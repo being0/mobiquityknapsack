74ac61b7b45c6aa9fd4de5df57f9646ec396d37b
reza <reza@hyperoffice.com>
2020-11-30 07:26:22 +0100

    repository test

--- src/main/java/com/mobiquity/packer/PackerConstraints.java
+++ src/main/java/com/mobiquity/packer/PackerConstraints.java
@@ -6,34 +6,34 @@ import java.math.BigDecimal;
  * @author <a href="mailto:raliakbari@gmail.com">Reza Aliakbari</a>
  * @version 1, 11/29/2020
  */
-public class PackerConstraints {
+class PackerConstraints {
 
     private final BigDecimal maxWeightConstraint;
     private final Integer maxNumberOfItemsConstraint;
     private final BigDecimal maxItemCostConstraint;
     private final BigDecimal maxItemWeightConstraint;
 
-    public PackerConstraints(BigDecimal maxWeightConstraint, Integer maxNumberOfItemsConstraint,
-                             BigDecimal maxItemCostConstraint, BigDecimal maxItemWeightConstraint) {
+    PackerConstraints(BigDecimal maxWeightConstraint, Integer maxNumberOfItemsConstraint,
+                      BigDecimal maxItemCostConstraint, BigDecimal maxItemWeightConstraint) {
         this.maxWeightConstraint = maxWeightConstraint;
         this.maxNumberOfItemsConstraint = maxNumberOfItemsConstraint;
         this.maxItemCostConstraint = maxItemCostConstraint;
         this.maxItemWeightConstraint = maxItemWeightConstraint;
     }
 
-    public BigDecimal getMaxWeightConstraint() {
+    BigDecimal getMaxWeightConstraint() {
         return maxWeightConstraint;
     }
 
-    public Integer getMaxNumberOfItemsConstraint() {
+    Integer getMaxNumberOfItemsConstraint() {
         return maxNumberOfItemsConstraint;
     }
 
-    public BigDecimal getMaxItemCostConstraint() {
+    BigDecimal getMaxItemCostConstraint() {
         return maxItemCostConstraint;
     }
 
-    public BigDecimal getMaxItemWeightConstraint() {
+    BigDecimal getMaxItemWeightConstraint() {
         return maxItemWeightConstraint;
     }
 }
--- src/test/groovy/com/mobiquity/packer/repository/KnapsackProblemFileRepositorySpec.groovy
+++ src/test/groovy/com/mobiquity/packer/repository/KnapsackProblemFileRepositorySpec.groovy
@@ -1,7 +1,44 @@
 package com.mobiquity.packer.repository
 
+
+import com.mobiquity.packer.model.KnapsackProblem
 import spock.lang.Specification
 
 class KnapsackProblemFileRepositorySpec extends Specification {
 
+    KnapsackProblemFileRepository repository;
+    LineParser lineParser;
+
+    def setup() {
+        lineParser = Stub(LineParser)
+    }
+
+
+    def 'Boundary case: test an empty file, repository should return empty list'() {
+        given:
+        repository = new KnapsackProblemFileRepository(getClass().getResource('/empty').getPath(), lineParser)
+        when:
+        List<KnapsackProblem> items = repository.readAll()
+        then:
+        items.size() == 0
+    }
+
+    def 'ParseException when file is not there'() {
+        given:
+        repository = new KnapsackProblemFileRepository("/notExistsPath", lineParser)
+        when:
+        repository.readAll()
+        then:
+        thrown(ParseException)
+    }
+
+    def 'File with 3 items'() {
+        given:
+        repository = new KnapsackProblemFileRepository(getClass().getResource('/three_problems').getPath(), lineParser)
+        lineParser.parse() >> Arrays.asList(new KnapsackProblem(new BigDecimal("100"), Collections.emptyList()))
+        when:
+        List<KnapsackProblem> items = repository.readAll()
+        then:
+        items.size() == 3
+    }
 }
