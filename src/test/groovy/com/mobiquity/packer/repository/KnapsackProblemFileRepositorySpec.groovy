dfda37ccf5627d931fbb7f400924b1630c7ecf15
reza <reza@hyperoffice.com>
2020-12-01 07:40:38 +0100

    more repository tests

--- src/test/groovy/com/mobiquity/packer/repository/KnapsackProblemFileRepositorySpec.groovy
+++ src/test/groovy/com/mobiquity/packer/repository/KnapsackProblemFileRepositorySpec.groovy
@@ -1,6 +1,5 @@
 package com.mobiquity.packer.repository
 
-
 import com.mobiquity.packer.model.KnapsackProblem
 import spock.lang.Specification
 
@@ -14,7 +13,7 @@ class KnapsackProblemFileRepositorySpec extends Specification {
     }
 
 
-    def 'Boundary case: test an empty file, repository should return empty list'() {
+    def 'readAll:Boundary case: test an empty file, repository should return empty list'() {
         given:
         repository = new KnapsackProblemFileRepository(getClass().getResource('/empty').getPath(), lineParser)
         when:
@@ -23,7 +22,7 @@ class KnapsackProblemFileRepositorySpec extends Specification {
         items.size() == 0
     }
 
-    def 'RepositoryException when file is not there'() {
+    def 'readAll:RepositoryException when file is not there'() {
         given:
         repository = new KnapsackProblemFileRepository("/notExistsPath", lineParser)
         when:
@@ -32,7 +31,7 @@ class KnapsackProblemFileRepositorySpec extends Specification {
         thrown(RepositoryException)
     }
 
-    def 'File with 3 items'() {
+    def 'readAll:File with 3 items'() {
         given:
         repository = new KnapsackProblemFileRepository(getClass().getResource('/three_problems').getPath(), lineParser)
         lineParser.parse() >> Arrays.asList(new KnapsackProblem(new BigDecimal("100"), Collections.emptyList()))
@@ -40,5 +39,35 @@ class KnapsackProblemFileRepositorySpec extends Specification {
         List<KnapsackProblem> items = repository.readAll()
         then:
         items.size() == 3
+    }
+
+    def 'executeOnAllEntries:Boundary case: test an empty file, repository should return empty list'() {
+        given:
+        repository = new KnapsackProblemFileRepository(getClass().getResource('/empty').getPath(), lineParser)
+        when:
+        int counter = 0;
+        repository.executeOnAllEntries({ problem -> counter++ })
+        then:
+        counter == 0
+    }
+
+    def 'executeOnAllEntries:RepositoryException when file is not there'() {
+        given:
+        repository = new KnapsackProblemFileRepository("/notExistsPath", lineParser)
+        when:
+        repository.executeOnAllEntries({ problem -> problem.items })
+        then:
+        thrown(RepositoryException)
+    }
+
+    def 'executeOnAllEntries:File with 3 items'() {
+        given:
+        repository = new KnapsackProblemFileRepository(getClass().getResource('/three_problems').getPath(), lineParser)
+        lineParser.parse() >> Arrays.asList(new KnapsackProblem(new BigDecimal("100"), Collections.emptyList()))
+        when:
+        int counter = 0;
+        repository.executeOnAllEntries({ problem -> counter++ })
+        then:
+        counter == 3
     }
 }
